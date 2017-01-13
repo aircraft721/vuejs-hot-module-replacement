8d3dc936955bb95e93054ce20fcc6e3630d5c7eb
Dan Constantinescu <dan_constantinescu@outlook.com>
2017-01-13 20:52:58 +0200

    lesson 17 - Hot module

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <counter></counter>
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import counter from './components/counter.vue'
+
+export default {
+  name: 'app',
+  components: { counter }
+}
+</script>
+
